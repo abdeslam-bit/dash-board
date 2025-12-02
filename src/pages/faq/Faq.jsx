e84038e51354b2dec91a161c9a88677248a660ea
abdeslam-bit <abdeslam12abdellani@gmail.com>
2025-12-02 22:47:39 +0100

    complete faq section

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -1,12 +1,94 @@
 import React from "react";
 import { useContext } from "react";
 import { ThemeContext } from "../../App";
+
+// mui
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Stack from "@mui/material/Stack";
+
 export default function Faq() {
   const { Ctheme } = useContext(ThemeContext);
   return (
     <div className={Ctheme === "dark" ? "dark" : ""}>
-      <div className="dark:text-white">Faq page</div>
-      <h1 className="dark:text-white">hello</h1>
+      <Stack gap={2}>
+        <Accordion
+          sx={{
+            bgcolor: Ctheme === "dark" ? "#1e293b" : "",
+            color: Ctheme === "dark" ? "white" : "black",
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography component="span">faq 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          sx={{
+            bgcolor: Ctheme === "dark" ? "#1e293b" : "",
+            color: Ctheme === "dark" ? "white" : "black",
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">faq 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          sx={{
+            bgcolor: Ctheme === "dark" ? "#1e293b" : "",
+            color: Ctheme === "dark" ? "white" : "black",
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">faq 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          sx={{
+            bgcolor: Ctheme === "dark" ? "#1e293b" : "",
+            color: Ctheme === "dark" ? "white" : "black",
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">faq 4</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+      </Stack>
     </div>
   );
 }
